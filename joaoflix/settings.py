0f15b839f4cbf2c1368d7dc88a876c053147409f
jatonini <joaoatonini@hotmail.com>
2023-08-14 11:57:30 -0300

    debug para falso

--- joaoflix/settings.py
+++ joaoflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s9@30dz^a5yvdilnhg2zp^8(an91w#%#qm12-v&quqj#k$*$we'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
